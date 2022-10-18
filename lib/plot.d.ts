23f62e6a38ca81998a5f03af0e01d4b8046dd92a
Grace Llewellyn <grace.llewellyn@jpl.nasa.gov>
2022-10-17 18:23:09 -0700

    converted back to original ipycmc from maap-jupyter-ide

--- lib/plot.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function generatePlotCommand(commandInfo: {
-    plotType: string;
-    startDate: Date;
-    endDate: Date;
-    geometry: any;
-    datasets: Array<string>;
-}): string;
